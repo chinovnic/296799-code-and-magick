31c4067f7d8458cb09d8777767f216b55d75f574
cusoq <cusoq@mail.ru>
2018-09-07 23:36:01 +0300

    Move constant announcement, create maxTime variable

--- js/stat.js
+++ js/stat.js
@@ -9,31 +9,35 @@ var BAR_WIDTH = 40;
 var MAX_BAR = 150;
 var CLOUD_COORDINATES_X = 100;
 var CLOUD_COORDINATES_Y = 10;
+var TIME_FONT_HEIGHT_RATE = 1.6;
+var NAME_FONT_HEIGHT_RATE = 0.5;
+var SHIFT = 10;
+var FIRST_LINE_POSITION_RATE = 2;
+var SECOND_LINE_POSITION_RATE = 3;
+var ALPHA_OPACITY_RATE = 0.2;
 var shadowCoordinatesX = CLOUD_COORDINATES_X + 10;
 var shadowCoordinatesY = CLOUD_COORDINATES_Y + 10;
+var maxTime;
 
 var renderCloud = function (ctx, x, y, color) {
-  var shift = 10;
   ctx.fillStyle = color;
   ctx.beginPath();
   ctx.moveTo(x, y);
-  ctx.lineTo(x + CLOUD_WIDTH / 4, y + shift);
+  ctx.lineTo(x + CLOUD_WIDTH / 4, y + SHIFT);
   ctx.lineTo(x + CLOUD_WIDTH / 2, y);
-  ctx.lineTo(CLOUD_WIDTH, y + shift);
+  ctx.lineTo(CLOUD_WIDTH, y + SHIFT);
   ctx.lineTo(x + CLOUD_WIDTH, y);
-  ctx.lineTo(x + CLOUD_WIDTH - shift, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x + CLOUD_WIDTH - SHIFT, y + CLOUD_HEIGHT / 2);
   ctx.lineTo(x + CLOUD_WIDTH, y + CLOUD_HEIGHT);
-  ctx.lineTo(CLOUD_WIDTH, y + CLOUD_HEIGHT - shift);
+  ctx.lineTo(CLOUD_WIDTH, y + CLOUD_HEIGHT - SHIFT);
   ctx.lineTo(x + CLOUD_WIDTH / 2, y + CLOUD_HEIGHT);
-  ctx.lineTo(x + CLOUD_WIDTH / 4, y + CLOUD_HEIGHT - shift);
+  ctx.lineTo(x + CLOUD_WIDTH / 4, y + CLOUD_HEIGHT - SHIFT);
   ctx.lineTo(x, y + CLOUD_HEIGHT);
-  ctx.lineTo(x + shift, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x + SHIFT, y + CLOUD_HEIGHT / 2);
   ctx.fill();
   ctx.closePath();
 };
 var renderText = function (ctx) {
-  var FIRST_LINE_POSITION_RATE = 2;
-  var SECOND_LINE_POSITION_RATE = 3;
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillStyle = '#000';
@@ -41,19 +45,17 @@ var renderText = function (ctx) {
   ctx.fillText('Список результатов:', CLOUD_X, FONT_HEIGHT * SECOND_LINE_POSITION_RATE);
 };
 var getMaxTime = function (times) {
-  var maxTime = times[0];
+  var currentMaxTime = times[0];
   for (var i = 1; i < times.length; i++) {
-    if (times[i] > maxTime) {
-      maxTime = times[i];
+    if (times[i] > currentMaxTime) {
+      currentMaxTime = times[i];
     }
   }
-  return maxTime;
+  return currentMaxTime;
 };
 var getBarColor = function (currentName) {
   var barColor = 'rgba(255, 0, 0, 1)';
-  console.log(currentName);
   if (currentName !== 'Вы') {
-    var ALPHA_OPACITY_RATE = 0.2;
     var alpha = Math.random();
     if (alpha < ALPHA_OPACITY_RATE) {
       alpha += ALPHA_OPACITY_RATE;
@@ -63,10 +65,8 @@ var getBarColor = function (currentName) {
   return barColor;
 };
 var renderBar = function (ctx, names, times, index) {
-  var TIME_FONT_HEIGHT_RATE = 1.6;
-  var NAME_FONT_HEIGHT_RATE = 0.5;
   var time = parseInt(times[index], 10);
-  var barHeight = MAX_BAR * times[index] / getMaxTime(times);
+  var barHeight = MAX_BAR * times[index] / maxTime;
   var barIndent = CLOUD_X + index * (GAP + BAR_WIDTH);
   ctx.fillStyle = '#000'; ctx.fillText(names[index], barIndent, CLOUD_Y);
   ctx.fillStyle = getBarColor(names[index]);
@@ -79,6 +79,7 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_COORDINATES_X, CLOUD_COORDINATES_Y, '#fff');
   renderText(ctx);
   for (var i = 0; i < names.length; i++) {
+    maxTime = getMaxTime(times);
     renderBar(ctx, names, times, i);
   }
 };
